39168fcd5f3a5ca1f92ce792319b4cfdc046cc5b
Pragyan Prusty <99946206+Pragyan-Prusty6@users.noreply.github.com>
2023-02-23 19:33:04 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,187 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height =( canvas.height = window.innerHeight);
+const para = document.querySelector('p');
+function random(min,max) {
+    const num = Math.floor(Math.random()*(max-min+1))+min;
+    return num;
+}
+function randomColor() {
+    return `rgb(${random(0,255)},255,${random(0,255)})`;
+}
+class Shape{
+  constructor(x,y,velX,velY){
+    this.x= x;
+    this.y= y;
+    this.velX= velX;
+    this.velY= velY;
+    
+  }
+}
+class BlackHole extends Shape{
+  constructor(x,y,color='black',size=50){
+    super(x,y,20,20);
+    this.color=color;
+   this.size=size;
+   window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "a":
+        this.x -= this.velX;
+        break;
+      case "d":
+        this.x += this.velX;
+        break;
+      case "w":
+        this.y -= this.velY;
+        break;
+      case "s":
+        this.y += this.velY;
+        break;
+    }
+  });
+}
+draw(){
+  ctx.beginPath();
+  ctx.lineWidth=4;
+  ctx.strokeStyle = this.color;
+  //x of center,y of center,radius,start radians,end radians
+  ctx.arc(this.x,this.y,this.size,0,2*Math.PI);
+  ctx.stroke();
+
+}
+checkBounds(){
+  if (((this.x + this.size) >= width)||((this.x - this.size) <= 0)
+  ||((this.y + this.size) >= height)||((this.y - this.size) <= 0)){
+   
+    this.size = -(this.size);
+  }
+ 
+}
+collisionDetect() {
+  for (const particle of particles) {
+    if (particle.exists) {
+      const dx = this.x - particle.x;
+      const dy = this.y - particle.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      //Collision detected
+      if (distance < this.size + particle.size) {
+        particle.color = this.color = 'transparent';
+        particle.exists=false;
+        numParticles--;
+        para.textContent = "Particle count: " + numParticles;
+        
+      }
+    }
+  }
+}
+}
+class Particle extends Shape{
+    constructor(x,y,velX,velY,color,size,exists=true){
+       super(x,y,velX,velY);
+        this.color=color;
+        this.size=size;
+        this.exists=exists;
+
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        //x of center,y of center,radius,start radians,end radians
+        ctx.arc(this.x,this.y,this.size,0,2*Math.PI);
+        ctx.fill();
+
+    }
+    update() {
+      //we include the size of the ball in the calculation
+      // because the x/y coordinates are in the center of the ball, but we want the
+      // edge of the ball to bounce off the perimeter
+      
+      //x-coordinate is greater than width of canvas
+      //i.e the particle is going off the right edge
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
+      }
+      collisionDetect() {
+        for (const particle of particles) {
+          if (this !== particle && particle.exists) {
+            const dx = this.x - particle.x;
+            const dy = this.y - particle.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            //Collision detected
+            if (distance < this.size + particle.size) {
+              particle.color = this.color = randomColor();
+            }
+          }
+        }
+      }
+      
+}   
+
+const particles = [];
+
+while (particles.length < 10) {
+  const size = random(15, 20);
+  const particle = new Particle(
+   
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-5, 5),
+    random(-5, 5),
+    randomColor(),
+    size
+  );
+
+  particles.push(particle);
+}
+let numParticles=particles.length;
+para.textContent = "Particle count: " + numParticles;
+function loop() {
+  //Sets the canvas fill color to semi-transparent black,
+  // then draws a rectangle of the color 
+  //across the whole width and height of the
+  // canvas, using fillRect()
+  //  This serves to cover up the 
+  //previous frame's drawing before the next one is drawn
+    ctx.fillStyle = "rgba(0, 140, 200,0.5)";
+    ctx.fillRect(0, 0, width, height);
+    const blackHole = new BlackHole(400,350);
+   
+
+    for (const particle of particles) {
+      if(particle.exists){
+        particle.draw();
+        particle.update();
+        particle.collisionDetect();
+      
+      }
+      blackHole.draw();
+      blackHole.checkBounds();
+      blackHole.collisionDetect();
+      
+
+    }
+  // when this method is repeatedly run and passed the same function name
+  //, it runs that function a set number of times per second to create a smooth animation. 
+  //This is generally done recursively — which means that the function is calling itself 
+  //every time it runs, so it runs over and over again
+    requestAnimationFrame(loop);
+  }
+loop();
